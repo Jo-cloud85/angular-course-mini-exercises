54d19d716607d114d609f1f38ba5f8c3a058e574
Jo-cloud85 <joan_tta@live.com.sg>
2024-04-01 17:09:08 +0800

    00_setting up

--- 00_understanding-basics/src/app/app.component.ts
+++ 00_understanding-basics/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '00_understanding-basics';
+
+  // For assignment 2
+  username = '';
+
+  // For assignment 3
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date())
+  }
 }
