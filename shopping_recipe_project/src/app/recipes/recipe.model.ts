08e1a65cef3ee72cdce80844dabd33046a8e4e60
Jo-cloud85 <joan_tta@live.com.sg>
2024-04-02 19:29:41 +0800

    01_setting up

--- shopping_recipe_project/src/app/recipes/recipe.model.ts
+++ shopping_recipe_project/src/app/recipes/recipe.model.ts
@@ -1,11 +1,19 @@
 export class Recipe {
-    public name: string;
-    public description: string;
-    public imagePath: string;
 
-    constructor(name: string, desc: string, imagePath: string) {
+    // public name: string;
+    // public description: string;
+    // public imagePath: string;
+
+    // constructor(name: string, desc: string, imagePath: string) {
+    //     this.name=name;
+    //     this.description=desc;
+    //     this.imagePath=imagePath;
+    // }
+
+    // Typescript shortcut for writing the same thing as above
+    constructor(public name: string, public description: string, public imagePath: string) {
         this.name=name;
-        this.description=desc;
+        this.description=description;
         this.imagePath=imagePath;
     }
 
