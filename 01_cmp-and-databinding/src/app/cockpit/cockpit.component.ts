1a68e6db7c7ee8e96d2d5c18fe26e697f34643cf
Jo-cloud85 <joan_tta@live.com.sg>
2024-04-02 23:10:11 +0800

    01_second commit

--- 01_cmp-and-databinding/src/app/cockpit/cockpit.component.ts
+++ 01_cmp-and-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,22 +7,43 @@ import { Component } from '@angular/core';
 })
 export class CockpitComponent {
 
-  newServerName = '';
-  newServerContent = '';
+  /* Instead of using @Input() (for properties you can mark outside), we want to do the opposite for both properties: serverCreated and blueprintCreated. We want to make sure that both are events we can emit.
+  And to make them events, we have to assign a new value to these properties i.e. new EventEmitter. And new EventEmitter is a generic type and we add () at the end, to call the constructor to create a new EventEmitter object. */
+  @Output()
+  serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') // adding alias - optional
+  blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // once you use local reference and pass in a paramter into your methods, you don't need this variable anymore
+  // newServerName = ''; 
+  // newServerContent = '';
 
-  onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+  @ViewChild('serverContentInput', {static: true}) // links to the html
+  serverContentInput: ElementRef;
+
+  
+  /* With the 2 methods below, we are emitting our own events and passing the data. By adding the @Output() annotation, we are allowing the 2 properties: serverCreated and blueprintCreated, listenable fromm the outside. Rmb, it is @Output, not @Input, because we are passing our own event out of the component. */
+
+
+  /* Using @ViewChild and then get the nativeElement, we get direct access to elements in our DOM, in our template. So basically w/o using two-way binding, but with local references passed to methods or local references fetched through @ViewChild. */
+    
+
+  onAddServer(nameInput: HTMLInputElement) {
+    // This will emit a new event of this type i.e. serverCreated type
+
+    // console.log(nameInput);
+    // console.log(this.serverContentInput);
+
+    this.serverCreated.emit({
+      // serverName: this.newServerName,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
   }
 
-  onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
   }
 }
--- 01_cmp-and-databinding/src/app/server-element/server-element.component.ts
+++ 01_cmp-and-databinding/src/app/server-element/server-element.component.ts
@@ -1,14 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css'
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges{
   // By default, all properties of components are only accessible inside these components
   // You have to be explicit about the properties you want to expose to the outside world
   // You want the parent components, in this case app.component, to be able to bind to this property, you need to add @Input().
   @Input('srvElement')
   element: {type: string, name: string, content:string};
+
+  // Understanding component lifecycle
+  // Get called 1st
+  constructor() {
+    console.log('Constructor is called');
+  }
+
+  // Get called 2nd
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log('ngOnChanges is called');
+      // this will be the 'element' object above that we created at the start
+      console.log(changes); 
+  }
+
+  // Get called 3rd
+  ngOnInit(): void {
+      console.log('ngOnInit is called');
+  }
 }
