5e654c82820e9d6cb8ca0cab5c8eb12cdf6db5ae
Jo-cloud85 <joan_tta@live.com.sg>
2024-04-05 14:26:25 +0800

    03_add services

--- 03_serv-and-dep-injection/src/app/account/account.component.ts
+++ 03_serv-and-dep-injection/src/app/account/account.component.ts
@@ -1,18 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input} from '@angular/core';
+
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  // constructor(private loggingService: LoggingService,
+  //             private accountsService: AccountsService) {}
+
+  // onSetTo(status: string) {
+  //   this.accountsService.updateStatus(this.id, status);
+  //   this.loggingService.logStatusChange(status);
+  // }
+
+
+  /* So now we are not building any chain of property and event binding. We still
+  have cross component communication through a service with the event emitter. */
+
+  constructor(private accountsService: AccountsService) {
+
+  }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id, status);
+    /* Since we inject the accountsService here where we set the new status, and 
+    then call accountsService.statusUpdated.emit - we are emitting an event I set  
+    up in the service. */
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- 03_serv-and-dep-injection/src/app/new-account/new-account.component.ts
+++ 03_serv-and-dep-injection/src/app/new-account/new-account.component.ts
@@ -1,18 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from './../logging.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  /* This informs Angular that we will need an instance of this LoggingService */
+  // constructor(private loggingService: LoggingService,
+  //             private accountsService: AccountsService) {}
+
+  // onCreateAccount(accountName: string, accountStatus: string) {
+  //   this.accountsService.addAccount(accountName, accountStatus);
+  //   this.loggingService.logStatusChange(accountStatus);
+  // }
+
+  constructor(private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    )
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.accountsService.statusUpdated.emit(accountStatus);
   }
 }
