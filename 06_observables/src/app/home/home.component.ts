8cfde2fe5fe8694ead420f107eee9352687d388b
Jo-cloud85 <joan_tta@live.com.sg>
2024-05-15 23:46:28 +0800

    update 06

--- 06_observables/src/app/home/home.component.ts
+++ 06_observables/src/app/home/home.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription!: Subscription;
 
+  constructor() {}
+
+  ngOnInit() {
+    /* interval is an observable we import from rxjs.
+
+    The idea of storing the subscription here simply is that we can now implement onDestroy here, and
+    inside of ngOnDestroy, we can now use our subscription and call unsubscribe. And that means whenever
+    we leave that component i.e. navigate away like clicking on another link so that we leave the current
+    component, we clear that subscription, therefore preventing memory leaks because we are not keeping
+    old subscriptions around. 
+    
+    The next question you might ask is why we don't have to unsubscribe the params observable in 
+    user.component.ts? params is an observable provided by Angular (it is not from rxjs), thus, Angular
+    will automatically do the unsubscribing for us once we leave the component. */
+
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
+
+    /* Customizing our own observable - although it is very rare that we do so, this is just to understand
+    how observables work behind the scenes. For example, any errors will cancel observer.complete(),. */
+    const customIntervalObservable = Observable.create((observer: any) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      filter((data: any) => {
+        return data > 0;
+      }), 
+      map((data: number) => {
+        return 'Round: ' + (data + 1);
+      }))
+      .subscribe((data: any) => {
+        console.log(data);
+      }, (error: any) => {
+        console.log(error);
+        alert(error.message);
+      }, () => {
+        console.log('Completed!');
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
--- 06_observables/src/app/user/user.component.ts
+++ 06_observables/src/app/user/user.component.ts
@@ -12,6 +12,15 @@ export class UserComponent {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    /* This is using an observable params to which we subscribe. 
+    
+    Observables are constructs to which you subscribe to be informed about changes in data. Because 
+    remember, observables are that stream of data and whenever a new data piece is emitted, our subscription 
+    will know about it. So in this case here, params is the observable. It's a stream of route parameters and
+    that stream gives us a new route parameter whenever we go to a new page i.e. whenever that parameter in 
+    the URL changes and then here in this function we pass to subscribe, we get the new params and we can
+    extract our ID param. */
+
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
